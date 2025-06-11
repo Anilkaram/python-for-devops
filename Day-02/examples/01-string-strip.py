b2aa11b7f2ecf15a4e46d2fabeb772804af389de
Anilkumar Karampuri <96483744+Anilkaram@users.noreply.github.com>
2025-06-11 17:11:57 +0530

    Update 01-string-strip.py

--- Day-02/examples/01-string-strip.py
+++ Day-02/examples/01-string-strip.py
@@ -1,3 +1,4 @@
 text = "   Some spaces around   "
 stripped_text = text.strip()
 print("Stripped text:", stripped_text)
+##Return a copy of the string with leading and trailing whitespace removed.If chars is given and not None, remove characters in chars instead.
